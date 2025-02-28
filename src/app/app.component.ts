0c37386f2eb7a13fa796db252c197800c422c62c
dinhminh0307 <dinhminh181003@gmail.com>
2025-03-01 00:26:01 +0700

    feat: separate module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
+import { NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms'; // ✅ Import FormsModule
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SearchBoxComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app';
+
+  handleSearch(query: string) {
+    console.log('Searching for:', query);
+    // Call API to fetch GitHub users or repositories
+  }
 }
